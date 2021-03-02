02e5f3c23a1ca8e8f1240bc2906f894077cd51e5
DatGuard <67444984+DatGuard@users.noreply.github.com>
2021-03-02 20:00:57 +0200

    Add files via upload

--- questions.js
+++ questions.js
@@ -2,193 +2,193 @@ questions = [
     {
         "question": "Green Day is based",
         "effect": {
-            "basd": -10
+            "econ": -10
         }
     },
     {
         "question": "Rape should be illegal",
         "effect": {
-            "basd": -10
+            "econ": -10
         }
     },
     {
         "question": "Niggers should be killed",
         "effect": {
-            "basd": 10
+            "econ": 10
         }
     },
     {
         "question": "Jews are poggers",
         "effect": {
-            "basd": -10
+            "econ": -10
         }
     },
     {
         "question": "The armenian genocide happened",
         "effect": {
-            "basd": -10
+            "econ": -10
         }
     },
     {
         "question": "Have you seen the Zohan 2008",
         "effect": {
-            "basd": 10
+            "econ": 10
         }
     },
     {
         "question": "You often feel the need to oppress minorities",
         "effect": {
-            "basd": 10
+            "econ": 10
         }
     },
     {
         "question": "when the impostor is sus! ඞ",
         "effect": {
-            "basd": 10
+            "econ": 10
         }
     },
     {
         "question": "Anime tiddies are based",
         "effect": {
-            "basd": 10
+            "econ": 10
         }
     },
     {
         "question": "shrek and batman epic hentai sex",
         "effect": {
-            "basd": 10
+            "econ": 10
         }
     },
     {
         "question": "femboy hooters.",
         "effect": {
-            "basd": 10
+            "econ": 10
         }
     },
     {
         "question": "FREE BALASTIN!!!!!1!",
         "effect": {
-            "basd": -10
+            "econ": -20
         }
     },
     {
         "question": "weezer cool poggers epicchamp pepecool",
         "effect": {
-            "basd": 10
+            "econ": 10
         }
     },
     {
         "question": "bibi king israel",
         "effect": {
-            "basd": 10
+            "econ": 10
         }
     },
     {
         "question": "ඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞ",
         "effect": {
-            "basd": 10
+            "econ": 10
         }
     },
     {
         "question": "i hate women",
         "effect": {
-            "basd": 10
+            "econ": 10
         }
     },
     {
         "question": "i have sex",
         "effect": {
-            "basd": -10
+            "econ": -10
         }
     },
     {
         "question": "i use dudeodorant",
         "effect": {
-            "basd": -10
+            "econ": -10
         }
     },
     {
         "question": "The phrase “it’s just a game” is such a weak mindset. You are ok with what happened, losing, imperfection of a craft. When you stop getting angry after losing, you’ve lost twice.",
         "effect": {
-            "basd": 10
+            "econ": 10
         }
     },
     {
         "question": "hydrogen poroxide",
         "effect": {
-            "basd": 10
+            "econ": 10
         }
     },
     {
         "question": "weak people stop smoking, real ones fight cancer",
         "effect": {
-            "basd": 10
+            "econ": 10
         }
     },
     {
         "question": "我非常喜歡華為，您應該購買它，並扮演騰訊製造的《PokémonUnite》，這是中國科技巨頭進軍遊戲機領域的最新舉措，並將其直接影響力擴展到了自己的總部之外 PogChamp 我非常喜歡華為，您應該購買它，並扮演騰訊製造的《PokémonUnite》，這是中國科技巨頭進軍遊戲機領域的最新舉措，並將其直接影響力擴展到了自己的總部之外 PogChamp 我非常喜歡華為，您應該購買它，並扮演騰訊製造的《PokémonUnite》，這是中國科技巨頭進軍遊戲機領域的最新舉措，並將其直接影響力擴展到了自己的總部之外 PogChamp",
         "effect": {
-            "basd": 10
+            "econ": 10
         }
     },
     {
         "question": "itamar ben gai",
         "effect": {
-            "basd": -10
+            "econ": -10
         }
     },
     {
         "question": "how far can you squirt",
         "effect": {
-            "basd": 10
+            "econ": 10
         }
     },
     {
         "question": "meinkampf funnis",
         "effect": {
-            "basd": 10
+            "econ": 10
         }
     },
     {
         "question": "Women of reddit(18F), what is the most sexual sexy sex sex sexual sexy sexual sexiest sexier sex Thing you have ever done to an antisocial 36 year old man living with his parents and spends 16 hours a day browsing reddit? Edit: Thank you for the gold kind stranger!",
         "effect": {
-            "basd": 10
+            "econ": 10
         }
     },
     {
         "question": "Reddit, am I the arsehole? My mum (82F) told me (12M) to do the dishes (16) but I (12M) was too busy playing Fortnite (3 kills) so I (12M) grabbed my controller (DualShock 4) and threw it at her (138kph). She fucking died, and I (12M) went to prison (18 years). While in prison I (12M) incited several riots (3) and assumed leadership of a gang responsible for smuggling drugs (cocaine) into the country. I (12M) also ordered the assassination of several celebrities (Michael Jackson, Elvis Presley and Jeffrey Epstein) and planned a terrorist attack (9/11). Reddit, AITA?",
         "effect": {
-            "basd": 10
+            "econ": 10
         }
     },
     {
         "question": "itamar ben gai 2",
         "effect": {
-            "basd": -10
+            "econ": -10
         }
     },
     {
         "question": "queen gergin",
         "effect": {
-            "basd": -10
+            "econ": -10
         }
     },
     {
         "question": "lenafd dalrymple hamlit on",
         "effect": {
-            "basd": 10
+            "econ": 10
         }
     },
     {
         "question": "fortnite sex (tech nigga)",
         "effect": {
-            "basd": 10
+            "econ": 10
         }
     },
     {
         "question": "shalom, i am here to take your foreskin (virginity)",
         "effect": {
-            "basd": 10
+            "econ": 10
         }
     }
 ];
